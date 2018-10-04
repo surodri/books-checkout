87d148ee51c4c6d8d4807cbf2e4a291a8ad51d4b
Susana Rodriguez <surod22@gmail.com>
2018-10-04 16:01:46 -0400

    1: [Susana] Updated Dockerfile to use pipenv for book service

--- book-domain-service/boot.sh
+++ book-domain-service/boot.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-source venv/bin/activate
-flask db upgrade
-flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
+exec gunicorn -b 0.0.0.0:5000 app_service:app
