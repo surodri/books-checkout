fb252b8fcf5025c005af52a0b2765511a9638f3d
Susana Rodriguez <surod22@gmail.com>
2018-10-08 09:33:37 -0400

    1: [Susana] Checkout endpoint retriving book from database

--- book-domain-service/app/routes.py
+++ book-domain-service/app/routes.py
@@ -1,10 +1,14 @@
 from app import app
 from flask import request, Response
+from app.book import Book
 
 @app.route('/<int:id>/checkout', methods=['PUT'])
 def checkout(id):
 
-    if(id == 1):
-        return Response("You checked out book id:" + str(id), 200)
+    bookTitle = Book.query.get(id).title
+
+    if(bookTitle):
+        return Response("You checked out book id: " + str(id) + " name: " +
+        str(bookTitle), 200)
     else:
         return Response("I wish we had that book! Try another", 404)
